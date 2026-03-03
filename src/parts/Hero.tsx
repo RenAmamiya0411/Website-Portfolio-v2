a9427ba56dd2de7acaa475e47093ce80076ffadb
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-03-04 06:25:14 +0800

    Created a template for Hero and Navbar.tsx

--- src/parts/Hero.tsx
+++ src/parts/Hero.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section className="h-screen bg-black flex flex-col items-center justify-center text-white px-8">
+      <h1 className="text-6xl font-bold text-center">
+        Hello, my name is <span className="text-red-500">Joker</span>
+      </h1>
+      <p className="mt-4 max-w-xl text-center text-gray-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, earum. Hic eligendi placeat, non id labore at.
+        Eius magni nam velit commodi at quaerat autem, deserunt obcaecati, rerum, explicabo laboriosam?
+      </p>
+      <a className="mt-8 px-6 py-3 bg-red-500 text-white font-bold hover:bg-red-600 transition-colors"href="#projects">View My Work</a>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/parts/Navbar.tsx
+++ src/parts/Navbar.tsx
@@ -2,20 +2,37 @@ import { Briefcase, Code, Mail, User } from "lucide-react";
 
 function Navbar() {
   return (
-    <nav className="hidden md:flex left-0 top-0 h-full w-16 bg-black flex-col items-center justify-center gap-8 z-50">
-      <a className="navbar-icons" href="#about">
-        <User size={24} />
-      </a>
-      <a className="navbar-icons" href="#skills">
-        <Code size={24} />
-      </a>
-      <a className="navbar-icons" href="#projects">
-        <Briefcase size={24} />
-      </a>
-      <a className="navbar-icons" href="#contact">
-        <Mail size={24} />
-      </a>
-    </nav>
+    <>
+      <nav className="hidden md:flex left-0 top-0 h-full w-16 bg-black flex-col items-center justify-center gap-8 z-50">
+        <a className="navbar-icons" href="#about">
+          <User size={24} />
+        </a>
+        <a className="navbar-icons" href="#skills">
+          <Code size={24} />
+        </a>
+        <a className="navbar-icons" href="#projects">
+          <Briefcase size={24} />
+        </a>
+        <a className="navbar-icons" href="#contact">
+          <Mail size={24} />
+        </a>
+      </nav>
+
+      <nav className="md:hidden fixed bottom-0 left-0 w-full bg-black flex justify-around items-center py-4 z-50">
+        <a className="navbar-icons" href="#about">
+          <User size={24} />
+        </a>
+        <a className="navbar-icons" href="#skills">
+          <Code size={24} />
+        </a>
+        <a className="navbar-icons" href="#projects">
+          <Briefcase size={24} />
+        </a>
+        <a className="navbar-icons" href="#contacts">
+          <Mail size={24} />
+        </a>
+      </nav>
+    </>
   );
 }
 
