fce6a0e7e22768e9ca0fadc3c091786590ebd3b5
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-03-04 02:25:38 +0800

    Imported template files into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import About from "./parts/About";
+import Contact from "./parts/Contact";
+import Hero from "./parts/Hero";
+import Navbar from "./parts/Navbar";
+import Projects from "./parts/Projects";
+import Skills from "./parts/Skills";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
