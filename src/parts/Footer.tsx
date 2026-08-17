69625f25062a8001834b6b16c8893547ef17857d
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-08-17 15:45:59 +0800

    feat: change name in footer

--- src/parts/Footer.tsx
+++ src/parts/Footer.tsx
@@ -4,11 +4,11 @@ function Footer() {
       <div className="max-w-3xl mx-auto flex flex-col md:flex-row items-center justify-between gap-4">
         <p className="text-gray-400 text-sm">
           © {new Date().getFullYear()}
-          <span className="text-red-500 font-bold"> Joker</span>
+          <span className="text-red-500 font-bold"> Steven Falcon</span>
         </p>
         <p className="text-gray-400 text-sm">
           Designed and Built by
-          <span className="text-red-500 font-bold"> Joker</span>
+          <span className="text-red-500 font-bold"> Steven Falcon</span>
         </p>
       </div>
     </footer>
